ab8f67e23b6404e6b49c82d5d1fea0171f1809da
Zhangning <benhzn07@gmail.com>
2015-04-15 20:03:27 -0400

    add real time bus

--- dataproject/urls.py
+++ dataproject/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
+from django.contrib import admin,auth
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,4 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^traffic/', include('traffic.urls', namespace = 'traffic')),
+    # # Authentication
+    # url(r'^login/$', auth.views.login),
+    # url(r'^logout/$', auth.views.logout),
 )
