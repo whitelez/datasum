3b547480c729ee8d56b47eae3a51c009810dd2dc
Bread <yangsg1026@gmail.com>
2015-05-28 18:09:47 -0400

    Merge branch 'master' of ec2-52-1-172-127.compute-1.amazonaws.com:/home/ec2-user/djcode/dataproject into SGYang
    Conflicts:
    	traffic/urls.py
    	traffic/views.py

--- dataproject/settings.py
+++ dataproject/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'NAME': 'dataproject',
 	    'USER': 'root',
 	    'PASSWORD': 'dataproject',
-        #'HOST': '52.1.172.127',
-        'HOST': 'LOCALHOST',
+        'HOST': '52.1.172.127',
+        #'HOST': 'LOCALHOST',
         'PORT': '3306',
 
     }
