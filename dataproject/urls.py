ea157ff2ac0adfa578a694213307f9cd82c2c833
Xidong Pi <xpi@andrew.cmu.edu>
2015-10-13 16:58:57 -0400

    make a new homepage for transit

--- dataproject/urls.py
+++ dataproject/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin,auth
 admin.autodiscover()
 
@@ -10,6 +9,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^traffic/', include('traffic.urls', namespace = 'traffic')),
+    url(r'^transit/', 'traffic.views.index2', name='index2'),
     # # Authentication
     # url(r'^login/$', auth.views.login),
     # url(r'^logout/$', auth.views.logout),
