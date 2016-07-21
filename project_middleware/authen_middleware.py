14686b54b35b5e26ad56505a95e0d7cb5bff34d9
whitelez <whitelez@outlook.com>
2016-07-20 22:37:53 -0400

    test2

--- project_middleware/authen_middleware.py
+++ project_middleware/authen_middleware.py
@@ -1,4 +1,4 @@
-# author: ryanwittgagagag
+# author: ryanwittgagagaghahahah
 # github address: https://gist.github.com/ryanwitt/130583#file-login_required_middleware-py
 
 from django.http import HttpResponseRedirect
